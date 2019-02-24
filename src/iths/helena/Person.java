ac1366ec2ced5b9d116465139d162e87f305497e
Helena <helena@bulle.nu>
2019-02-24 17:01:31 +0100

    Added a person class

---
+++ src/iths/helena/Person.java
@@ -0,0 +1,11 @@
+package iths.helena;
+
+public class Person {
+    private String fname;
+    private String lname;
+
+    public Person(String fname, String lname) {
+        this.fname = fname;
+        this.lname = lname;
+    }
+}
