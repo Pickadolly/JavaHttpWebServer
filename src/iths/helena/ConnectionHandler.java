5a0f48f89333cdee2507556ed06dcff2abd3b396
Helena <helena@bulle.nu>
2019-02-24 17:24:49 +0100

    Changed so that the class now works

--- src/iths/helena/ConnectionHandler.java
+++ src/iths/helena/ConnectionHandler.java
@@ -7,18 +7,22 @@ import java.net.Socket;
 public class ConnectionHandler extends Thread { //by extending to thread this class becomes a thread
 
     private Socket s;
+
+    //for sending the output to client
     PrintWriter pw;
+
+    //for getting the input from client
     BufferedReader br;
-    BufferedOutputStream dataOut = null;
+
+    BufferedOutputStream bos = null;
 
     //constructor
     public ConnectionHandler(Socket s) throws IOException {
         this.s = s;
         br = new BufferedReader(new InputStreamReader(s.getInputStream()));
         pw = new PrintWriter(s.getOutputStream());
-        dataOut = new BufferedOutputStream(s.getOutputStream());
+        bos = new BufferedOutputStream(s.getOutputStream());
     }
-
 
     //thread class contains a method run which is call automatically when we start the thread
     //in this method we have to read the request and give the response
@@ -27,27 +31,37 @@ public class ConnectionHandler extends Thread { //by extending to thread this cl
         try {
             //in this method we get the request string and give this string to the request class
             String reqS = "";
+            StringBuilder sb = new StringBuilder();
 
             //from br we have to read our request
             //read until request is not get or br is ready
-            while (br.ready() || reqS.length() == 0)
-                reqS += (char) br.read();
-            System.out.println(reqS); //for display
-            HttpRequest req = new HttpRequest(reqS);
+            while (br.ready() || sb.length() == 0)
+                sb.append((char) br.read());
+            System.out.println("----------------------------------------------------");
+            System.out.println("HTTP REQUEST:");
+            System.out.println(sb.toString()); //for display
 
-            //now we pass the http request object to the response class
-            HttpResponse res = new HttpResponse(req);
-
-            //write the final output to pw
-            pw.write(res.response.toCharArray());
-            pw.flush();
-            if(res.data == null){
-            } else {
-                dataOut.write(res.data, 0, res.fileLength);
-                dataOut.flush();
+            try {
+                HttpRequest req = new HttpRequest(sb.toString());
+                //now we pass the http request object to the response class
+                HttpResponse res = new HttpResponse(req);
+                //write the final output to pw
+                System.out.println("----------------------------------------------------");
+                System.out.println("HTTP RESPONSE:");
+                System.out.println(res.response.toCharArray());
+                pw.write(res.response.toCharArray());
+                pw.flush();
+                if (res.data == null) {
+                } else {
+                    bos.write(res.data, 0, res.fileLenght);
+                    bos.flush();
+                }
+                bos.close();
+                br.close();
+                s.close();
+            } catch (Exception a){
+                a.printStackTrace();
             }
-            br.close();
-            s.close();
         } catch (Exception e){
             e.printStackTrace();
         }
